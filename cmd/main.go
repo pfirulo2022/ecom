6853f9f4dfbf2c6ecfa4774afafedb27e57deb5b
Pablo Firulo <pfirulo@yahoo.com>
2024-04-09 14:01:05 -0300

    conectando a db

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/pfirulo2022/ecom/cmd/api"
+	"github.com/pfirulo2022/ecom/config"
+	"github.com/pfirulo2022/ecom/db"
+)
+
+func main() {
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := api.NewAPIServer(":8080", db)
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
